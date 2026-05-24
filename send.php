caa6506b4acdfb165c2303b4ccf61ee2e29cae78
FATHER <153096951+therealfather@users.noreply.github.com>
2026-05-24 18:25:58 +0300

    Update send.php

--- send.php
+++ send.php
@@ -13,6 +13,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = htmlspecialchars($_POST['name'] ?? '');
     $email = htmlspecialchars($_POST['email'] ?? '');
     $message = htmlspecialchars($_POST['message'] ?? '');
+    $phone = htmlspecialchars($_POST['phone'] ?? '');
     
     // Validare
     if (empty($name) || empty($email) || empty($message)) {
